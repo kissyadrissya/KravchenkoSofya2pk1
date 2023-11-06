e9e1c5bb6818f4d7728cf9802387538a61732224
kissyadrissya <coticusbaby@gmail.com>
2023-11-06 20:34:02 +0500

    Добавлена пятая практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PZ_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод числа N
+            Console.Write("Введите целое число N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Вычисление двойного факториала
+            double doubleFactorial = 1;
+            int i = n;
+            while (i >= 1)
+            {
+                doubleFactorial *= i;
+                i -= 2;
+            }
+
+            // Вывод результата
+            Console.WriteLine($"Двойной факториал числа {n}: {doubleFactorial}");
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
+
+
